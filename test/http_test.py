728d406d27b41a57e2af1aa937c90b281eada1f0
La_fe <37364197+La-fe@users.noreply.github.com>
2024-06-16 22:26:10 +0800

    update: Add test cases

--- test/http_test.py
+++ test/http_test.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from llama_index.core.multi_modal_llms.generic_utils import (
     encode_image,
@@ -5,8 +7,6 @@ from llama_index.core.multi_modal_llms.generic_utils import (
 )
 from pydantic import BaseModel
 from rich import print
-
-baseUrl = "http://127.0.0.1:8001"
 
 
 def test_embeddings():
@@ -29,14 +29,13 @@ def test_embeddings():
     print(response.json().keys())
 
 
-def image_embeddings():
+def clip_image_embeddings(image_path: str):
+    if image_path is None:
+        image_path = "../docs/images/image-1.png"
     model_name = "clip"
     base_url = "http://localhost:50110"
 
-    request_body = {
-        "model": model_name,
-        "image": encode_image("../docs/images/query.png"),
-    }
+    request_body = {"model": model_name, "image": encode_image(image_path)}
 
     response = requests.post(
         url=f"{base_url}/api/clip-image-embeddings",
@@ -48,14 +47,30 @@ def image_embeddings():
     print(response.json())
 
 
-def image_ocr():
+def clip_text_embeddings():
+    model_name = "clip"
+    base_url = "http://localhost:50110"
+    prompt = "hello "
+
+    request_body = {"model": model_name, "prompt": prompt}
+
+    response = requests.post(
+        url=f"{base_url}/api/clip-text-embeddings",
+        headers={"Content-Type": "application/json"},
+        json=request_body,
+    )
+
+    print(response)
+    print(response.json())
+
+
+def image_ocr(image_path):
+    if image_path is None:
+        image_path = "../docs/images/image-1.png"
     model_name = "ocr"
     base_url = "http://localhost:50110"
 
-    request_body = {
-        "model": model_name,
-        "image": encode_image("../docs/images/query.png"),
-    }
+    request_body = {"model": model_name, "image": encode_image(image_path)}
 
     response = requests.post(
         url=f"{base_url}/api/image-ocr",
@@ -65,6 +80,9 @@ def image_ocr():
 
     print(response)
     print(response.json())
+
+
+baseUrl = "http://127.0.0.1:8001"
 
 
 def test_build_databse():
@@ -95,10 +113,70 @@ def test_notes_query():
     print(response.json())
 
 
+def lifecycle_clip_image_embeddings(image_path: str):
+    if image_path is None:
+        image_path = "../docs/images/image-1.png"
+    model_name = "clip"
+    base_url = "http://localhost:8000"
+
+    request_body = {"model": model_name, "image": encode_image(image_path)}
+
+    response = requests.post(
+        url=f"{base_url}/api/clip-image-embeddings",
+        headers={"Content-Type": "application/json"},
+        json=request_body,
+    )
+
+    print(response)
+    print(response.json())
+
+
+def set_model():
+    # 设置请求的URL
+    url = "http://localhost:50110/api/set-keep-alive"
+
+    # 设置请求的数据
+    data = {
+        "model_name": "all",
+        "keep_alive": 100,  # 设置持有时间为600秒
+        "hold_forever": False,  # 设置为非永久持有
+    }
+
+    # 发送POST请求
+    response = requests.post(url, json=data)
+
+    # 打印响应
+    print(response.json())
+
+
+def lifecycle_set_model():
+    # 设置请求的URL
+    url = "http://localhost:8000/api/set-model-config"
+
+    # 设置请求的数据
+    data = {
+        "hold_time": 5,  # 设置持有时间为600秒
+        "hold_forever": False,  # 设置为非永久持有
+    }
+
+    # 发送POST请求
+    response = requests.post(url, json=data)
+
+    # 打印响应
+    print(response.json())
+
+
 if __name__ == "__main__":
+    image_path = "../assets/medias/6370c90a000000001501704b_0.jpg"
+    # lifecycle_clip_image_embeddings(image_path)
+
     # test_build_databse()
     # test_notes_search()
     # test_notes_query()
-    image_ocr()
-    # image_embeddings()
+    # image_ocr(image_path)
+    clip_image_embeddings(image_path)
+    # clip_text_embeddings()
     # test_embeddings()
+
+    # set_model()
+    # lifecycle_set_model()
